61f58f98ab4ca61b94f5e1200aa92faef4cdb86f
JimmyCapps <125122130+JimmyCapps@users.noreply.github.com>
2024-03-11 11:01:02 +1100

    Cleansed personal details

--- DADLANCHER.bat
+++ DADLANCHER.bat
@@ -4,7 +4,7 @@ color 0A
 
 
 :begin
-cd "C:\Users\JamesCapplis\Desktop\DADLAN"
+cd "C:\Users\usernane\Desktop\DADLAN"
 type ASCIILOGO.txt
 echo.
 echo Testing Wireguard Connection:
@@ -147,7 +147,7 @@ goto Menu1
 
 :op5
 echo Quake 3
-"C:\Users\JamesCapplis\Downloads\Games\Quake3\Quake3\quake3e-vulkan.x64.exe"
+"C:\Users\username\Downloads\Games\Quake3\Quake3\quake3e-vulkan.x64.exe"
 CLS
 goto Menu1
 
