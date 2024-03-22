dc95330f89e1c37009088c7e1536def43664a2f7
MrCaffeen <MrCaffeen@users.noreply.github.com>
2024-03-23 00:28:08 +1100

    functions
    uploading DADLAN.ps1 main script calling two sub scripts functions then menu
    PSSystrayLauncher.ps1 for Tray menu
    suppfunctions.ps1 for all functions and testing

---
+++ DADLAN.ps1
@@ -0,0 +1,17 @@
+# Name   : DADLAN.ps1
+# Author : MrCaffeen, 
+# Date   : 20/03/2024
+# Note   : Conversion of Batch file for DADLAN/DADWAN use for application network and game controls
+
+#====================================================================================================================
+# LOAD SUPPLEMENTARY FUNCTIONS,
+function Get-ScriptDirectory {
+	$Invocation = (Get-Variable MyInvocation -Scope 1).Value
+	Split-Path $Invocation.MyCommand.Path
+	# TO TEST - See http://stackoverflow.com/questions/801967/how-can-i-find-the-source-path-of-an-executing-script
+	# ABOVE MIGHT BE BETTER WRITTEN AS:
+	# Split-Path $script:MyInvocation.MyCommand.Path
+}
+# dot source our supplementary functions file
+'C:\DADLAN\scripts\suppFunctions.ps1'
+'C:\DADLAN\scripts\PSSystrayLauncher.ps1'
